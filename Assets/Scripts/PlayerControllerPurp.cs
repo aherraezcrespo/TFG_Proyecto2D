6b05fc37faee2f52f99e6ccdd2d9a483462f17a9
Alejandro <78168951+aherraezcrespo@users.noreply.github.com>
2022-05-16 11:01:27 +0200

    HablidadesPlayers

--- Assets/Scripts/PlayerControllerPurp.cs
+++ Assets/Scripts/PlayerControllerPurp.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerPurp : MonoBehaviour
 {
     private float speed = 5;
     private float movementX;
     private SpriteRenderer flipPlayer;
     private Rigidbody2D playerRb;
-    private float jumbForce = 7;
+    private float jumbForce = 9;
     private bool isGround = true;
     private Animator animatorPlayerJump;
     private Animator animatorPlayerRun;
@@ -25,20 +25,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         movementX = Input.GetAxis("Horizontal");
 
         if (movementX > 0)
         {
-            
+
             flipPlayer.flipX = false;
         }
-        
+
         if (movementX < 0)
         {
             flipPlayer.flipX = true;
         }
-        
+
         transform.position += new Vector3(movementX, 0, 0) * speed * Time.deltaTime;
 
         animatorPlayerRun.SetFloat("VelocidadMovimiento", Mathf.Abs(movementX));
@@ -48,7 +47,6 @@ public class PlayerController : MonoBehaviour
             animatorPlayerJump.SetTrigger("Jump");
             playerRb.AddForce(Vector2.up * jumbForce, ForceMode2D.Impulse);
         }
-
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -63,6 +61,7 @@ public class PlayerController : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
 
     private void OnCollisionExit2D(Collision2D collision)
     {
---
+++ Assets/Scripts/PlayerControllerRed.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerRed : MonoBehaviour
+{
+    private float speed = 5;
+    private float movementX;
+    private SpriteRenderer flipPlayer;
+    private Rigidbody2D playerRb;
+    private float jumbForce = 7;
+    private bool isGround = true;
+    private Animator animatorPlayerJump;
+    private Animator animatorPlayerRun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flipPlayer = GetComponent<SpriteRenderer>();
+        playerRb = GetComponent<Rigidbody2D>();
+        animatorPlayerRun = GetComponent<Animator>();
+        animatorPlayerJump = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementX = Input.GetAxis("Horizontal");
+
+        if (movementX > 0)
+        {
+
+            flipPlayer.flipX = false;
+        }
+
+        if (movementX < 0)
+        {
+            flipPlayer.flipX = true;
+        }
+
+        transform.position += new Vector3(movementX, 0, 0) * speed * Time.deltaTime;
+
+        animatorPlayerRun.SetFloat("VelocidadMovimiento", Mathf.Abs(movementX));
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGround)
+        {
+            animatorPlayerJump.SetTrigger("Jump");
+            playerRb.AddForce(Vector2.up * jumbForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGround = true;
+        }
+
+        if (collision.gameObject.tag == "Water")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGround = false;
+        }
+    }
+}
